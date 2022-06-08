7a9bdb0c09b3613d0e0febee8cb7845e537fe753
isabelafr696 <105250155+isabelafr696@users.noreply.github.com>
2022-06-08 00:42:27 -0300

    Update app.py

--- Churn/app.py
+++ Churn/app.py
@@ -1,6 +1,5 @@
 import cloudpickle
 from flask import Flask, render_template, request
-import numpy as np
 
 with open('model.pkl', 'rb') as file_in:
   model = cloudpickle.load(file_in)
@@ -8,23 +7,28 @@ with open('model.pkl', 'rb') as file_in:
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html', nome='Fulano')
+def homepage():
+  return render_template('homepage.html', nome='Fulano')
 
 @app.route('/predicao', methods=['POST'])
 def predicao():
-    int_features=[int(x) for x in request.form.values()]
-    genero = request.form['genero']
-    idade = request.form['idade']
-    telefone= request.form['telefone']
-    suporte= request.form['suporte']
-    streaming= request.form['streaming']
-    predicao = model.predict([genero],[idade],[telefone],[suporte],[streaming])
-    return render_template('resposta.html', predicao=predicao[0])
-
+  Gender = int(request.form['Gender'])
+  Senior_Citizen = int(request.form['Senior_Citizen'])
+  Phone_Service = int(request.form['Phone_Service'])
+  Tech_Support = int(request.form['Tech_Support'])
+  Streaming_Movies = int(request.form['Streaming_Movies'])
+  Contract = int(request.form['Contract'])
+  Payment_Method = int(request.form['Payment_Method'])
+  predicao = model.predict([[Gender, Senior_Citizen, Phone_Service, Tech_Support, Streaming_Movies, Contract, Payment_Method]])
+  return render_template('predicao.html', predicao=predicao[0])
+  
+  
 app.run(debug=True)
 
+# pip install -r requirements.txt (instala as bibliotecas)
+# python app.py (para executar)
 
+# git add .
 
-
-
+# git commit -m "nomenovo"
+# git push
